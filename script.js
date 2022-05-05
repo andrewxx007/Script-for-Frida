8545afa14149605f6186b9e679997e3fb9eb2904
andrewxx007 <99376457+andrewxx007@users.noreply.github.com>
2022-05-05 04:07:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// @File: scripts/script.js
+
+setTimeout(function() {
+    Java.perform(function() {
+
+        // We are loading the class here
+        var Rfunc = Java.use("a.a.a.a.a");
+
+        // Whenever a.a.a.a.a.R() is called, the below code is executed
+        Rfunc.R.implementation = function() {
+            // Capture the return value of R function
+            let retval = this.R();
+            // Printing the value we got from this function
+            console.log("Original return value = ", retval);
+            // Toggle the return value to make sure it doesn't enter the if loop
+            return !retval
+        }
+    })
+}, 10);
